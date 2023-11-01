0428c0654940f2f94b0c8f85866ee3efde29aa8a
MjxOro <mattheweoro@gmail.com>
2023-11-01 18:21:22 -0400

    feat: setup redux to app file.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,17 @@
 import { TamaguiProvider } from 'tamagui';
 import config from './tamagui.config';
-import Navigator from './Navigator'
+import Navigator from './Navigator';
+import { Provider } from 'react-redux';
+import { store } from './store/index';
+import { useAppSelector } from './store/index';
 
 export default function App() {
+  const theme = useAppSelector((state) => state.theme);
   return (
-    <TamaguiProvider defaultTheme='light' config={config}>
-      <Navigator />
-    </TamaguiProvider>
+    <Provider store={store}>
+      <TamaguiProvider defaultTheme={theme.currentTheme} config={config}>
+        <Navigator />
+      </TamaguiProvider>
+    </Provider>
   );
 }
